171622e9d3dd1ac6d771ee0e70cd9ade0602cf3b
Gareth Healy <garethahealy@gmail.com>
2024-05-29 14:02:25 +0100

    added sorting to ldap to make printed out list easier to read

--- src/main/java/com/garethahealy/githubstats/services/users/GitHubMemberInRedHatLdapService.java
+++ src/main/java/com/garethahealy/githubstats/services/users/GitHubMemberInRedHatLdapService.java
@@ -78,6 +78,8 @@ public class GitHubMemberInRedHatLdapService {
             }
         }
 
+        Collections.sort(answer);
+
         logger.info("--> User Lookup DONE");
         return answer;
     }
