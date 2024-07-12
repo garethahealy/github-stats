f76532b834eed04924f64d031e980b843302f453
Gareth Healy <garethahealy@gmail.com>
2024-07-12 14:47:29 +0100

    added script to upload generated files to github repo

---
+++ scripts/collect-redhat-cop.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+pushd ~/Documents/Git/github.com/garethahealy/github-stats
+
+source creds.source
+
+./target/github-stats-*-runner users collect-members-from-ldap --organization=redhat-cop --csv-output=ldap-members.csv --ldap-members-csv=ldap-members.csv --fail-if-no-vpn=true --guess=false
+
+rm -rf member-mapping || true
+git clone git@github.com:redhat-cop-dev/member-mapping.git
+
+cp ldap-members.csv member-mapping/ldap-members.csv
+cp supplementary.csv member-mapping/supplementary.csv
+
+pushd ~/Documents/Git/github.com/garethahealy/github-stats/member-mapping
+
+git add *.csv
+git commit -m "updated members list"
+git push
+
+popd
+popd
