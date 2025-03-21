8c652abba439964b16f55c453c391ee27f039bd7
Gareth Healy <garethahealy@gmail.com>
2025-03-21 13:08:24 +0000

    excluded pre commit for docs

--- scripts/run-redhat-cop.sh
+++ scripts/run-redhat-cop.sh
@@ -8,7 +8,7 @@ source creds.source
 
 ./target/github-stats-*-runner users collect-members-from-ldap --organization=redhat-cop --ldap-members-csv=ldap-members.csv --supplementary-csv=supplementary.csv --validate-csv=false --fail-if-no-vpn=true >> output.log
 ./target/github-stats-*-runner users create-who-are-you-issues -dry-run=true --organization=redhat-cop --issue-repo=org --ldap-members-csv=ldap-members.csv --supplementary-csv=supplementary.csv --fail-if-no-vpn=true --permission=read --guess=true >> output.log
-./target/github-stats-*-runner users listen-to-issues --dry-run=true --organization=redhat-cop --issue-repo=org --processors=AddMeAsMember --ldap-members-csv=ldap-members.csv --supplementary-csv=supplementary.csv--fail-if-no-vpn=true >> output.log
+./target/github-stats-*-runner users listen-to-issues --dry-run=true --organization=redhat-cop --issue-repo=org --processors=AddMeAsMember --ldap-members-csv=ldap-members.csv --supplementary-csv=supplementary.csv --fail-if-no-vpn=true >> output.log
 ./target/github-stats-*-runner users listen-to-pullrequests --dry-run=true --organization=redhat-cop --issue-repo=org --processors=MembersChangeInConfigYaml,MembersChangeInAnsibleVarsYaml --ldap-members-csv=ldap-members.csv --supplementary-csv=supplementary.csv --fail-if-no-vpn=true >> output.log
 
 
