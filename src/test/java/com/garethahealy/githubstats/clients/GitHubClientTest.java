9577079908adc3d093399ebd5fa0e3d6ecb8b3f0
Gareth Healy <garethahealy@gmail.com>
2026-01-26 08:38:45 -0800

    update test for github client
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- src/test/java/com/garethahealy/githubstats/clients/GitHubClientTest.java
+++ src/test/java/com/garethahealy/githubstats/clients/GitHubClientTest.java
@@ -1,16 +1,33 @@
 package com.garethahealy.githubstats.clients;
 
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@QuarkusTest
 class GitHubClientTest {
 
+    @Inject
+    GitHubClient client;
+
     @Test
-    void getWriteClient() {
+    void getWriteClient() throws IOException {
+        GitHub github = client.getWriteClient();
+
+        assertNotNull(github);
+        assertNotNull(github.getRateLimit());
     }
 
     @Test
-    void getClient() {
+    void getClient() throws IOException {
+        GitHub github = client.getClient();
+
+        assertNotNull(github);
+        assertNotNull(github.getRateLimit());
     }
 }
