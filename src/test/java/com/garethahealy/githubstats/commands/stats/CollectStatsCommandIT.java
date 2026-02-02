e80494cc538b4c227d9ac9aa29251adce811e7fd
Gareth Healy <garethahealy@gmail.com>
2026-02-02 17:04:54 +0000

    updated tests to be Launcher based instead of executing command
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- src/test/java/com/garethahealy/githubstats/commands/stats/CollectStatsCommandIT.java
+++ src/test/java/com/garethahealy/githubstats/commands/stats/CollectStatsCommandIT.java
@@ -1,31 +1,22 @@
 package com.garethahealy.githubstats.commands.stats;
 
-import com.garethahealy.githubstats.commands.BaseCommand;
+import io.quarkus.test.junit.main.Launch;
+import io.quarkus.test.junit.main.LaunchResult;
+import io.quarkus.test.junit.main.QuarkusMainIntegrationTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledIf;
-import org.zeroturnaround.exec.ProcessExecutor;
-import org.zeroturnaround.exec.ProcessResult;
-
-import java.io.IOException;
-import java.util.concurrent.TimeoutException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-class CollectStatsCommandIT extends BaseCommand {
+@QuarkusMainIntegrationTest
+class CollectStatsCommandIT {
 
     @Test
-    @EnabledIf(value = "isRunnerSet")
-    void run() throws IOException, InterruptedException, TimeoutException {
-        ProcessExecutor executor = new ProcessExecutor()
-                .command(getRunner(), "stats", "collect-stats", "--organization=redhat-cop", "--csv-output=target/redhat-cop-collect-stats.csv", "--validate-org-config=true", "--repository-limit=5", "--api-limit=400")
-                .redirectError(System.err)
-                .redirectOutput(System.out);
+    @Launch(value = {"stats", "collect-stats", "--organization=redhat-cop", "--csv-output=target/redhat-cop-collect-stats.csv", "--validate-org-config=true", "--repository-limit=5", "--api-limit=400"})
+    void run(LaunchResult result) {
+        result.echoSystemOut();
 
-        String command = String.join(" ", executor.getCommand());
-        System.out.println("Executing \"" + command + "\"");
-
-        ProcessResult result = executor.execute();
-
-        assertEquals(0, result.getExitValue());
+        assertNotNull(result.getErrorOutput());
+        assertEquals(0, result.exitCode());
     }
 }
--- src/test/java/com/garethahealy/githubstats/commands/users/ListenToIssuesCommandIT.java
+++ src/test/java/com/garethahealy/githubstats/commands/users/ListenToIssuesCommandIT.java
@@ -1,31 +1,22 @@
 package com.garethahealy.githubstats.commands.users;
 
-import com.garethahealy.githubstats.commands.BaseCommand;
+import io.quarkus.test.junit.main.Launch;
+import io.quarkus.test.junit.main.LaunchResult;
+import io.quarkus.test.junit.main.QuarkusMainIntegrationTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledIf;
-import org.zeroturnaround.exec.ProcessExecutor;
-import org.zeroturnaround.exec.ProcessResult;
-
-import java.io.IOException;
-import java.util.concurrent.TimeoutException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-class ListenToIssuesCommandIT extends BaseCommand {
+@QuarkusMainIntegrationTest
+class ListenToIssuesCommandIT {
 
     @Test
-    @EnabledIf(value = "isRunnerSet")
-    void run() throws IOException, InterruptedException, TimeoutException {
-        ProcessExecutor executor = new ProcessExecutor()
-                .command(getRunner(), "users", "listen-to-issues", "--dry-run=true", "--organization=redhat-cop", "--issue-repo=org", "--processors=AddMeAsMember", "--ldap-members-csv=ldap-members.csv", "--supplementary-csv=supplementary.csv", "--fail-if-no-vpn=true")
-                .redirectError(System.err)
-                .redirectOutput(System.out);
+    @Launch(value = {"users", "listen-to-issues", "--dry-run=true", "--organization=redhat-cop", "--issue-repo=org", "--processors=AddMeAsMember", "--ldap-members-csv=ldap-members.csv", "--supplementary-csv=supplementary.csv", "--fail-if-no-vpn=true"})
+    void run(LaunchResult result) {
+        result.echoSystemOut();
 
-        String command = String.join(" ", executor.getCommand());
-        System.out.println("Executing \"" + command + "\"");
-
-        ProcessResult result = executor.execute();
-
-        assertEquals(0, result.getExitValue());
+        assertNotNull(result.getErrorOutput());
+        assertEquals(0, result.exitCode());
     }
 }
